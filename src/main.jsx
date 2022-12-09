526adf743a329831d2d611dc81db954f1727e561
felipe kettl <fekettl@gmail.com>
2022-12-09 20:39:23 -0300

    remove not used dependency

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './styles.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Home from './pages/Home'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home></Home>
